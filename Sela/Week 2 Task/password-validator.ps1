e2cca04bb3b1fe0e77c04cd534c14c3052984431
guy-lavan <85857310+guy-lavan@users.noreply.github.com>
2022-03-04 17:34:00 +0200

    powershell script
    powershell script

---
+++ Sela/Week 2 Task/password-validator.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host """Password Cheack - password should contain at least: one lower case letter, one upper case letter, One number and be over 10 chars long"""
+
+$s='aAA23as'
+
+Write-Host  "password is $s"
+# loop to check content of password var and validate it
+if (($s -cmatch '[a-z]') -and ($s -cmatch '[A-Z]') -and ($s -match '\d') -and ($s.length -ge 10)) 
+{
+       Write-Host "Pass! Password is Good!"
+        exit 0
+}
+else
+{
+        Write-Host "Fail! Password does not meet required pattern !"
+        exit 1
+}
